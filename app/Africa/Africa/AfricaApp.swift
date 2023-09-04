b5536a4eb5bedff9fa93548563808cc0c8f52feb
bryce0516 <jayhspark@gmail.com>
2023-09-04 03:48:01 +0000

    first commit

---
+++ app/Africa/Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by 박혜성(Hyesung Park) on 2023/09/04.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
