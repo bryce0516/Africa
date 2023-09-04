cfceb8e22687d9df8abbe2d90e1891e3d7c39386
bryce0516 <jayhspark@gmail.com>
2023-09-04 05:25:34 +0000

    add CoverImageView and decodable protocol

--- app/Africa/Africa/Africa/App/ContentView.swift
+++ app/Africa/Africa/Africa/App/ContentView.swift
@@ -8,19 +8,33 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Content!")
-        }
-        .padding()
+  var body: some View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    NavigationView {
+      List {
+        CoverImageView()
+          .frame(height: 300)
+          .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+      }
+      .navigationTitle("Africa")
+      .navigationBarTitleDisplayMode(.large)
+//
+      
     }
+    //        VStack {
+    //            Image(systemName: "globe")
+    //                .imageScale(.large)
+    //                .foregroundColor(.accentColor)
+    //            Text("Content!")
+    //        }
+    //        .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
