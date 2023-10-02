a2cbff7830da6b77c00c4ddf14750b58749348d4
hyesungpark <jayhspark@gmail.com>
2023-10-02 13:33:12 +0900

    finish video view

--- app/Africa/Africa/App/VideoListView.swift
+++ app/Africa/Africa/App/VideoListView.swift
@@ -8,8 +8,19 @@
 import SwiftUI
 
 struct VideoListView: View {
+  // MARK: - PROPERTIES
+  
+  var videos: [Video] = Bundle.main.decode("videos.json")
+  
+  // MARK: - BODY
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+      NavigationView {
+        List {
+          ForEach(videos) { video in
+            VideoListItem(video: video)
+          } //: FOREACH
+        } //: LIST
+      }
     }
 }
 
