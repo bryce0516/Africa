25c2e239f7efba2483ac90101bf65b2b7f5d195d
hyesungpark <jayhspark@gmail.com>
2023-10-02 21:30:45 +0900

    add gallery view

--- app/Africa/Africa/App/GalleryView.swift
+++ app/Africa/Africa/App/GalleryView.swift
@@ -8,18 +8,39 @@
 import SwiftUI
 
 struct GalleryView: View {
-    var body: some View {
-      ScrollView(.vertical, showsIndicators: false) {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-      } //: SCROLLVIEW
-      .frame(maxWidth: .infinity, maxHeight: .infinity)
-      .background(MotionAnimationView())
-
-    }
+  let animals: [Animal] = Bundle.main.decode("animals.json")
+//  let gridLayout: [GridItem] = [
+//    GridItem(.flexible()),
+//    GridItem(.flexible()),
+//    GridItem(.flexible())
+//  ]
+  let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+  
+  var body: some View {
+    ScrollView(.vertical, showsIndicators: false) {
+      // MARK: - GRID
+      LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10){
+        ForEach(animals) { item in
+          Image(item.image)
+            .resizable()
+            .scaledToFit()
+            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            .overlay(
+              Circle()
+                .stroke(Color.white, lineWidth: 1)
+            )
+        } //: LOOP
+      } //: GRID
+      
+    } //: SCROLLVIEW
+    .frame(maxWidth: .infinity, maxHeight: .infinity)
+    .background(MotionAnimationView())
+    
+  }
 }
 
 struct GalleryView_Previews: PreviewProvider {
-    static var previews: some View {
-        GalleryView()
-    }
+  static var previews: some View {
+    GalleryView()
+  }
 }
