109ce4ce3e63deccf6d7b4cbc7e13ec3457a3ec6
hyesungpark <jayhspark@gmail.com>
2023-10-02 21:55:48 +0900

    finish gallery view

--- app/Africa/Africa/App/GalleryView.swift
+++ app/Africa/Africa/App/GalleryView.swift
@@ -8,30 +8,69 @@
 import SwiftUI
 
 struct GalleryView: View {
+  
+  @State private var selectedAnimal: String = "lion"
+  
   let animals: [Animal] = Bundle.main.decode("animals.json")
+  let haptics = UIImpactFeedbackGenerator(style: .medium)
 //  let gridLayout: [GridItem] = [
 //    GridItem(.flexible()),
 //    GridItem(.flexible()),
 //    GridItem(.flexible())
 //  ]
-  let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+//  let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+  @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+  @State private var gridColumn: Double = 3.0
+  
+  func gridSwitch() {
+    gridLayout = Array(
+      repeating: .init(.flexible()),
+      count: Int(gridColumn)
+    )
+  }
   
   var body: some View {
     ScrollView(.vertical, showsIndicators: false) {
       // MARK: - GRID
-      LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10){
-        ForEach(animals) { item in
-          Image(item.image)
-            .resizable()
-            .scaledToFit()
-            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
-            .overlay(
-              Circle()
-                .stroke(Color.white, lineWidth: 1)
-            )
-        } //: LOOP
-      } //: GRID
-      
+      VStack(alignment: .center, spacing: 30) {
+        
+        Image(selectedAnimal)
+          .resizable()
+          .scaledToFit()
+          .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+          .overlay(Circle().stroke(Color.white, lineWidth: 8))
+        
+        Slider(value: $gridColumn, in: 2...4, step: 1)
+          .padding(.horizontal)
+          .onChange(of: gridColumn, perform: {
+            value in 
+            withAnimation(.easeIn) {
+              gridSwitch()
+            }
+          })
+        
+        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10){
+          ForEach(animals) { item in
+            Image(item.image)
+              .resizable()
+              .scaledToFit()
+              .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+              .overlay(
+                Circle()
+                  .stroke(Color.white, lineWidth: 1)
+              )
+              .onTapGesture {
+                selectedAnimal = item.image
+                haptics.impactOccurred()
+              }
+          } //: LOOP
+        } //: GRID
+        .onAppear(perform: {
+          gridSwitch()
+        })
+      }
+      .padding(.horizontal, 10)
+      .padding(.vertical, 50)
     } //: SCROLLVIEW
     .frame(maxWidth: .infinity, maxHeight: .infinity)
     .background(MotionAnimationView())
