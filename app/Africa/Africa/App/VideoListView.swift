7a891ce3268d7e1b668c90b324a32adb14a8fe60
hyesungpark <jayhspark@gmail.com>
2023-10-02 13:53:56 +0900

    finish video list view

--- app/Africa/Africa/App/VideoListView.swift
+++ app/Africa/Africa/App/VideoListView.swift
@@ -9,23 +9,37 @@ import SwiftUI
 
 struct VideoListView: View {
   // MARK: - PROPERTIES
+  @State var videos: [Video] = Bundle.main.decode("videos.json")
   
-  var videos: [Video] = Bundle.main.decode("videos.json")
-  
+  let hapticImpact = UIImpactFeedbackGenerator(style: .medium)
   // MARK: - BODY
-    var body: some View {
-      NavigationView {
-        List {
-          ForEach(videos) { video in
-            VideoListItem(video: video)
-          } //: FOREACH
-        } //: LIST
+  var body: some View {
+    NavigationView {
+      List {
+        ForEach(videos) { video in
+          VideoListItemView(video: video)
+            .padding(.vertical, 8)
+        } //: LOOP
+      } //: LIST
+      .listStyle(InsetGroupedListStyle())
+      .navigationTitle("Videos")
+      .navigationBarTitleDisplayMode(.inline)
+      .toolbar {
+        ToolbarItem(placement: .topBarTrailing) {
+          Button {
+            videos.shuffle()
+            hapticImpact.impactOccurred(intensity: <#T##CGFloat#>)
+          } label: {
+            Image(systemName: "arrow.2.squarepath")
+          }
+        }
       }
     }
+  }
 }
 
 struct VideoListView_Previews: PreviewProvider {
-    static var previews: some View {
-        VideoListView()
-    }
+  static var previews: some View {
+    VideoListView()
+  }
 }
